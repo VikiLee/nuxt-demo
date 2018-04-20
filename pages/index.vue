761e28f08865c1714ec2f47348ec4cd9c944b769
liweiji <liweiji@xunlei.com>
2018-04-20 15:02:47 +0800

    fetch获取数据，服务器代理获取

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
 <div class="wrap">
    hello {{username}}
+   <div class="wrap">
+    <h1>姓名：{{info.name}}</h1>
+      <h2>年龄：{{info.age}}</h2>
+      <h2>兴趣：{{info.interest}}</h2>
+  </div>
 </div>
 </template>
 <script>
@@ -8,6 +13,15 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'homepage',
+  //middleware: 'authenticated',
+  // async asyncData({req, res, query, store}) {
+  //   store.dispatch('getuserinfo')
+  //   var {data} = await axios('https://api.myjson.com/bins/1329lz', {})
+  //   return {info: data}
+  // },
+  fetch({store}) {
+    return store.dispatch('getuserinfo')
+  },
   data() {
     return {
     }
@@ -18,7 +32,10 @@ export default {
   computed: {
     ...mapState({
       username: state => state.userinfo.username || 'hello'
-    })
+    }),
+    info() {
+      return this.$store.state.userinfo
+    }
   },
   created() {
     this.getData({
